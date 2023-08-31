494d38fe88cb47973e5735fb33e7e870829b3b39
hiroya.shimamoto <shshimamo@gmail.com>
2023-08-31 14:58:08 +0900

    fix prettier config

--- frontend/main/prettier.config.js
+++ frontend/main/prettier.config.js
@@ -1,6 +1,6 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
   semi: false,
   singleQuote: true,
   printWidth: 90,
