9715d45409d19ea0c4fec48cfd37fa8653fea56b
hiroya.shimamoto <shshimamo@gmail.com>
2025-08-11 17:22:48 +0900

    e2eタイムアウト調整

--- frontend/main/playwright.config.ts
+++ frontend/main/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
-  timeout: 5000,
+  timeout: 10000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
